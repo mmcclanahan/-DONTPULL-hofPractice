76acc5b5bf36fd499a5de06b543a3e4378a6469f
mmcclanahan <mmmcclan365@gmail.com>
2023-12-12 16:43:49 -0800

    Completed Assingment

--- practice.js
+++ practice.js
@@ -64,17 +64,23 @@ var onlyOneFruit = function (fruits, targetFruit) {
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function (fruits, letter) {
-
+  return _.filter(fruits, function(fruit) {
+    return fruit[0] === letter;
+  });
 };
 
 // return a filtered array containing only cookie-type desserts.
 var cookiesOnly = function (desserts) {
-
+  return _.filter(desserts, function(dessertObj) {
+    return dessertObj['type'] === 'cookie';
+  });
 };
 
 // rebuild the getUserTweets function from above with _.filter instead
 var filterUserTweets = function(tweets, user) {
-
+  return _.filter(tweets, function(tweetObj) {
+    return tweetObj['user'] === user;
+  });
 };
 
 /*
@@ -86,20 +92,30 @@ var filterUserTweets = function(tweets, user) {
 // given an array of strings, use _.map to return a new array containing all
 // strings converted to uppercase letters.
 var upperCaseFruits = function (fruits) {
-
+  return _.map(fruits, function(fruit) {
+    return fruit.toUpperCase();
+  });
 };
 
 // given an array of dessert objects, return a new array of objects
 // that have a new "glutenFree" property, with a boolean value.
 // TIP: Items that contain flour are not gluten-free.
 var glutenFree = function (desserts) {
-
+  return _.map(desserts, function(dessertObj) {
+    dessertObj['glutenFree'] = false;
+    if (dessertObj['ingredients'].includes('flour')) {
+      dessertObj['glutenFree'] = true;
+    }
+    return dessertObj;
+  });
 };
 
 // given an array of tweet objects, return a new array of strings
 // containing only the message properties.
 var allUserMessages = function(tweets) {
-
+  return _.map(tweets, function(tweetObj) {
+    return tweetObj['message']
+  });
 };
 
 // use _.map to return an array of items with their sale prices, with a new property
@@ -110,7 +126,7 @@ var allUserMessages = function(tweets) {
 //
 /*
 
- example output:
+  example output:
   var salePrices = applyCoupon(groceries, 0.20);
   [
     {
@@ -123,7 +139,15 @@ var allUserMessages = function(tweets) {
 
 */
 var applyCoupon = function (groceries, coupon) {
-
+  //return array of items
+  //price:
+  return _.map(groceries, function(itemObject) {
+    var price = itemObject['price'].replace('$', '');
+    itemObject['salePrice'] = '$' + (price - (price * coupon)).toFixed(2);
+    return itemObject;
+  });
+  //salePrice:
+  //
 };
 
 /*
@@ -134,18 +158,28 @@ var applyCoupon = function (groceries, coupon) {
 
 // return the total price of all products.
 var sumTotal = function (products) {
-
+  return _.reduce(products, function(memo, itemObj) {
+    return Number(itemObj['price'].replace('$', 0)) + memo;
+  }, 0);
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
 var dessertCategories = function (desserts) {
-
+  var obj = {};
+  return _.reduce(desserts, function(memo, dessertObj) {
+    if (memo[dessertObj['type']] === undefined) {
+      memo[dessertObj['type']] = 1;
+    } else {
+      memo[dessertObj['type']] = memo[dessertObj['type']] + 1;
+    }
+    return memo;
+  }, obj);
 };
 
 // return an object with the proper count of all user messages
 /*
- example output:
+  example output:
   var tweetCountPerUser = countMessagesPerUser(tweets);
   {
     "douglascalhoun": 5,
@@ -155,19 +189,35 @@ var dessertCategories = function (desserts) {
   }
 */
 var countMessagesPerUser = function(tweets) {
-
+  return _.reduce(tweets, function(memo, tweetObj) {
+    memo[tweetObj['user']] = (memo[tweetObj['user']] || 0) + 1;
+    return memo;
+  }, {});
 };
 
 // given an array of movie data objects,return an array containing
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function (movies) {
-
+  return _.reduce(movies, function(memo, movieObj) {
+    //if movieObj[releaseYear] >= 1990 && <=2000
+      //push movieObj[title] to memo
+    var year = movieObj['releaseYear'];
+    if (year >= 1990 && year <= 2000) {
+      memo.push(movieObj['title']);
+    }
+    return memo;
+  }, []);
 };
 
 // return an boolean stating if there exists a movie with a shorter
 // runtime than your time limit.
 // timeLimit is an integer representing a number of minutes.
 var movieNight = function (movies, timeLimit) {
-
+  return _.reduce(movies, function(memo, movieObj) {
+    if (movieObj['runtime'] < timeLimit) {
+      memo = true;
+    }
+    return memo;
+  }, false);
 };
